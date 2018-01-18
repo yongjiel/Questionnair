1a6b9f66b0ab1613a93a49393014567f26f48200
Yongjie Liang <yongjiel@ualberta.ca>
2018-01-17 22:23:48 -0700

    Add radiobox type into questions. Make dynamic design in coding.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, FieldList, TextField, TextAreaField
+from wtforms import StringField, PasswordField, \
+                    BooleanField, SubmitField, FieldList,\
+                    RadioField, TextField, TextAreaField, FormField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import Question
 from app import db
 from app.models import Answer, Question
+from wtforms import Form as NoCsrfForm
+import sqlalchemy
+from flask import flash
+
+## to increase type of question table, 
+## must add it in this list and the dict below.
+ALLOWED_TYPES = ['textarea', 'radiobox']
+FIELD_TYPES = {'textarea': "TextAreaField",
+               'radiobox': 'RadioField'}
 
 
 def email_exist(form, field):
@@ -11,20 +22,84 @@ def email_exist(form, field):
     if ans:
         raise ValidationError('Email addess existed!. Please use other email address!')
 
+def type_restrict(form, field):
+    if not field.data in ALLOWED_TYPES:
+        print("fdsafdsafdsffdasf")
+        msg = 'Only ' + ",".join(ALLOWED_TYPES) + " allowed!"
+        flash(msg, category='error')
+        raise ValidationError(msg)
+    
+class QForm(FlaskForm):
+    ''' for question field list '''
+    class Meta:
+        csrf = False
+    # for flask db init
+    try:
+        questions = Question.query.all()
+    except sqlalchemy.exc.OperationalError as e:
+        questions = []
+    for i in range(len(questions)):
+        name = "question" + str(i+1)
+        if questions[i].type == ALLOWED_TYPES[0]:
+            cmd = name + " = " + FIELD_TYPES[ALLOWED_TYPES[0]] +"(\"Question\",  validators=[DataRequired()])"
+            exec(cmd)
+        elif questions[i].type == ALLOWED_TYPES[1]:
+            l = questions[i].choice
+            l = map(lambda x: "('" + x.strip() + "','" + x.strip() + "')", l.split(","))
+            s = ",".join(l)
+            cmd = name + " = " + FIELD_TYPES[ALLOWED_TYPES[1]] + "(\"Question\", choices=[" + s + "])"
+            exec(cmd)
+
+
+
 class QuestionForm(FlaskForm):
+    ''' for entire question form with username and submit'''
     # call all the questions from database.
-    questions = Question.query.all()
+    # for flask db init
+    try:
+        questions = Question.query.all()
+    except sqlalchemy.exc.OperationalError as e:
+        questions = []
     username = StringField("Username (Email address):  required", 
                             validators=[DataRequired(), 
                                         Email("Wrong format!"),
                                         email_exist])
-    
-    question_fields = FieldList(TextAreaField("Question", validators=[DataRequired()]), min_entries=12)
+    question_fields = FieldList(FormField(QForm), min_entries=1, max_entries=1)
+
     submit = SubmitField('Submit')
 
+class QEForm(FlaskForm):
+    ''' for question field list '''
+    class Meta:
+        csrf = False
+    # for flask db init
+    try:
+        questions = Question.query.all()
+    except sqlalchemy.exc.OperationalError as e:
+        questions = []
+    for i in range(len(questions)):
+        name = "question" + str(i+1)
+        cmd = name + " = TextField(\"Question\",  validators=[DataRequired()])"
+        exec(cmd)
+        name = "question_type" + str(i+1)
+        cmd = name + " = TextField(\"Type\",  validators=[DataRequired(), type_restrict])"
+        exec(cmd)
+        name = "question_choice" + str(i+1)
+        cmd = name + " = TextField(\"Choice\")"
+        exec(cmd)
+
 class QuestionEditForm(FlaskForm):
-    questions = Question.query.all()
-    question_fields = FieldList(TextField("Question", validators=[DataRequired()]), min_entries=12)
+    ''' for edit questions '''
+    # for flask db init
+    try:
+        questions = Question.query.all()
+    except sqlalchemy.exc.OperationalError as e:
+        questions = []
+    question_fields = FieldList(FormField(QEForm), min_entries=1, max_entries=1)
     submit = SubmitField('Submit')
+
+    
+
+        
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,94 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app, db
 from app.forms import QuestionForm, QuestionEditForm
 from app.models import Question, Answer
+import re
+
+array = {}
+
+def save_answer(dict, email):
+    for k, v in dict.iteritems():
+        m = re.search(r"question(\d+)", k)
+        if m:
+            id = m.group(1)
+            q = Question.query.get(id)
+            a = Answer(description=v, question=q, email=email)
+            db.session.add(a)
+    db.session.commit()
 
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = QuestionForm()
-    form.questions = Question.query.all()
-    if form.validate_on_submit():
+    form = QuestionForm(meta={'csrf': True})
+    if request.method == 'POST' and form.validate_on_submit():
         email = form.data['username']
-        qs = Question.query.all()
         for fieldname, value in form.data.items():
-            if fieldname == "question_fields":
-                for idx in range(len(value)):
-                    a = Answer(description=value[idx], question=qs[idx], email=email)
-                    db.session.add(a)
-                db.session.commit()
+            if fieldname == 'question_fields':
+                array[fieldname] = value[0]
+                #'question_fields': [{'csrf_token': u'', 'question11': u'Bus', 
+                                    #'question10': u'Edmonton', 'question12': u' Toronto', 
+                                    #'question5': u'sf', 'question4': u'dsafd', 'question7': u'fdsaf', 
+                                    # 'question6': u'dsa', 'question1': u'fdsafs', 'question3': u'dsaf', 
+                                    #'question2': u'fdsaf', 'question9': u'fdsfds', 'question8': u'dsaf'}]
+                save_answer(value[0], email)
         flash('Questions are submmited by user {}'.format(
             form.username.data))
         return redirect('/')
+    else:
+        form.questions = Question.query.all()
     return render_template('questionForm.html', title='Task1', form=form)
+
+def save_question(dict):
+    qs = Question.query.all()
+    for q in qs:
+        i = int(q.id)
+        for k, v in dict.iteritems():
+            m = re.search(r"question(\d+)", k)
+            if m and int(m.group(1)) == i:
+                q.description = v
+                break
+        for k, v in dict.iteritems():
+            m = re.search(r"question_type(\d+)", k)
+            if m and int(m.group(1)) == i:
+                q.type = v
+                break
+        for k, v in dict.iteritems():
+            m = re.search(r"question_choice(\d+)", k)
+            if m and int(m.group(1)) == i:
+                q.choice = v
+                break
+        db.session.add(q)
+    db.session.commit()
 
 
 @app.route("/admin/question", methods=['GET', 'POST'])
 def questions():
-    form = QuestionEditForm()
-    form.questions = Question.query.all()
-    if form.validate_on_submit():
+    form = QuestionEditForm(meta={'csrf': True})
+    if request.method == 'POST' and form.validate_on_submit():
         # save to database
+        #'question_fields': [{'question_choice8': u'', 'question_choice9': u'', 
+        #  'question_choice2': u'', 'question_choice3': u'', 'question_choice1': u'', 
+        #  'question_choice6': u'', 'question_choice7': u'', 'question_choice4': u'', 
+        #  'question_choice5': u'', 'question_choice10': u'Calgary,Edmonton,Airdry', 
+        #  'question_choice11': u'Car,Bus,Walk', 'question_choice12': u'Chicago, Toronto, Calgary', 
+        #  'question_type9': u'textarea', 'question_type8': u'textarea', 'question_type7': u'textarea', 
+        #  'question_type6': u'textarea', 'question_type5': u'textarea', 'question_type4': u'textarea', 
+        #  'question_type3': u'textarea', 'question_type2': u'textarea', 'question_type1': u'textarea', 
+        #  'question5': u'What were your mother born?', 'question4': u'What flight company did you use last time?', 
+        #  'question7': u'What is your favourite food?', 'question6': u'What is the model of your last car?', 
+        #  'question1': u'What is your favourite boots?', 'question3': u'Where were your born?', 
+        #  'question2': u'What is your favourite movie?', 'question9': u"What is your first pet's name?", 
+        #  'question8': u'Where were your father born?', 'question11': u'How do you go to work everyday?', 
+        #  'question10': u'Where are your working now?', 'question12': u'What is the last city you vistied?', 
+        #  'question_type12': u'radiobox', 'question_type11': u'radiobox', 'question_type10': u'radiobox'}]
         for fieldname, value in form.data.items():
-            if fieldname == "question_fields":
-                for idx in range(len(value)):
-                    q = Question.query.get(idx + 1)
-                    q.description = value[idx]
-                    db.session.add(q)
-                db.session.commit()
+            if fieldname == 'question_fields':
+                save_question(value[0])
         flash('Questions are update!')
         return redirect('/admin/question')
+    else:
+        form.questions = Question.query.all()
     return render_template('questionEditForm.html', title='Task1', form=form)
 
 @app.route("/admin/answer")
